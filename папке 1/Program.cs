2aa728d7e7650aa747a1fbf696c2522c8f34bc5d
Ognehodets <iliuie@yandex.by>
2023-09-18 22:03:56 +0300

    все задачи

---
+++ папке 1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sq = a*a;
+Console.WriteLine ($"квадрат числа {a} = {sq}");
---
+++ папке 2/Program.cs
@@ -0,0 +1,11 @@
+﻿int qr = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+if (a*a == qr)
+{
+    Console.WriteLine("true");
+}
+else 
+{
+    Console.WriteLine("false");
+}
+
